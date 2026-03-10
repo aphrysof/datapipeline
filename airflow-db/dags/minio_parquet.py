078b550b4330a24da389def2db26fadd9beb953f
Sofia Wanjiru Abdullahi <39647231+aphrysof@users.noreply.github.com>
2026-03-10 16:34:14 +0300

    Update DAG start date to January 1, 2025

--- airflow-db/dags/minio_parquet.py
+++ airflow-db/dags/minio_parquet.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 with DAG(
     dag_id="example_dag",
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime(2025, 1, 1),
     schedule_interval="@daily",
     catchup=False,
 ) as dag:
